35d71b8b93441ca72ea57327dcf982ffa0de729f
Furkan Karayel <f.karayel+github@icloud.com>
2025-02-10 04:52:07 +0100

    Add main file for starting with db and cache DI

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/furkankarayel/URL_Shortener/config"
+	"github.com/furkankarayel/URL_Shortener/internal/api"
+	"github.com/furkankarayel/URL_Shortener/internal/cache"
+	"github.com/furkankarayel/URL_Shortener/internal/db"
+	"github.com/furkankarayel/URL_Shortener/internal/urlshortener"
+)
+
+func main() {
+
+	// Set up routes
+	topLevelRoutes := make(map[string]*api.Route)
+
+	config, cfgErr := config.NewConfig(".env")
+	if cfgErr != nil {
+		log.Fatal("Failed to load config", cfgErr)
+	}
+
+	db, dbErr := db.NewDB(config)
+	if dbErr != nil {
+		log.Fatal("Failed to connect to database", dbErr)
+	}
+	urlCache := cache.NewURLCache()
+
+	urlCache.Save("test", "https://stackoverflow.com/quww")
+
+	log.Println(urlCache.Get("test"))
+	topLevelRoutes["url"] = urlshortener.New(db, urlCache)
+
+	svr := api.New(topLevelRoutes)
+	err := http.ListenAndServe(":8080", svr)
+	log.Println(err)
+	log.Println("Server is running")
+
+}
